a864927983e5aeeef83b957510d33a318099eb9a
Hailey Rae <haileyraehughes89@gmail.com>
2023-04-25 11:14:30 -0500

    successful parsing of data

--- script.js
+++ script.js
@@ -16,43 +16,45 @@ function geoCoding (cityName, stateCode, countryCode, limit) {
         fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon +'&appid=4699afd8fabd5888ac079efa9eff2bc3')
         
         .then(function(resp) { return resp.json() }) // Convert data to json
-        .then(function(data) {
+        .then(function sameday(data) {
+            for (i=0; i<=4; i++){
           console.log(data);
+          var windSpeed = data.list[0].wind.speed;
+          console.log('Wind Speed', windSpeed);
+          var temperature = data.list[0].main.temp;
+          console.log('Temperature', temperature);
+          var humidity = data.list[0].main.humidity;
+          console.log("humidity", humidity);
+          var windSpeed1 = data.list[1].wind.speed;
+          console.log('Wind Speed', windSpeed1);
+          var temperature1 = data.list[1].main.temp;
+          console.log('Temperature', temperature1);
+          var humidity1 = data.list[1].main.humidity;
+          console.log("humidity", humidity1);
+            }
         })
-      })
+        // .then(function (data) {
+        //     console.log(data);
+           
+        // })
+        // .then(function future(data) {
+        //     console.log(data);
+        //     var windSpeed = data.list[1].wind.speed;
+        //     console.log('Wind Speed', windSpeed);
+        //     var temperature = data.list[1].main.temp;
+        //     console.log('Temperature', temperature);
+        //     var humidity = data.list[1].main.humidity;
+        //     console.log("humidity", humidity);
+        //   })
+        })
+    }
+
+geoCoding ("'olathe'",'US-KS', 'US', 5) 
     
-    .then(function weather(location) {
-        var winds = location.wind[0]
-        console.log(winds);
-        return winds;
-    })
-}
 
-      //weather (0,100);)
-
-    // .catch(function(error) {
-    //     console.log(error);
-   
  
 
 
-geoCoding ("'overland park'",'US-KS', 'US', 5) 
-    
-
- 
-
-// function weather(lat,lon) {
-//     var key = '4699afd8fabd5888ac079efa9eff2bc3';
-//     fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon +'&appid=4699afd8fabd5888ac079efa9eff2bc3')  
-//     .then(function(resp) { return resp.json() }) // Convert data to json
-//     .then(function(data) {
-//       console.log(data);
-//     })
-//     .catch(function() {
-//       // catch any errors
-//     });
-//   }
-//   weather (0,100);
 
 
 
